377dce5a5f19c82ed10c2021f17623ef027b46ea
Armen <armenstepanyan20032023@gmail.com>
2025-09-05 20:36:10 +0400

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    async redirects() {
+        return [
+            {
+                source: '/', // The incoming request path
+                destination: '/blogs', // The path you want to redirect to
+                permanent: true, // Set to true for permanent redirects (308), false for temporary (307)
+            },
+        ];
+    },
 };
 
 export default nextConfig;
